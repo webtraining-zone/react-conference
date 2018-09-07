3f9b1f5b8214f923e848e12d494f3c58531fef38
Alex Arriaga <alexzander.arriaga@base22.com>
2018-09-07 15:20:57 -0500

    Agregar soporte para SASS

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import Header from "./components/Header/Header";
+import Program from "./components/Program/Program";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className="App">
+          <Header/>
+          <Program/>
+        </div>
     );
   }
 }
