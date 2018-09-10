cd6b109beef4c8ab25a40bf4df60da56c48fa6c7
Alex Arriaga <alexzander.arriaga@base22.com>
2018-09-10 15:13:40 -0500

    Agregar componentes de mapa y mensaje final

--- src/components/Home/HomeIndex.js
+++ src/components/Home/HomeIndex.js
@@ -1,10 +1,14 @@
 import React from "react";
 import HomeHero from "./HomeHero";
+import HomeMap from "./HomeMap";
+import HomeQuestions from "./HomeQuestions";
 
 const HomeIndex = () => {
   return (
       <div>
         <HomeHero/>
+        <HomeMap/>
+        <HomeQuestions/>
       </div>
   );
 };
---
+++ src/components/Home/HomeQuestions.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const HomeQuestions = (props) => {
+  return <section className="b-section b-section--highlighted">
+    <div className="container">
+      <div className="row">
+        <div className="col-8 offset-2">
+          <h3 className="text-center pt-5 pb-4">
+            No dudes en aclarar tus dudas, te proveeremos todo lo que necesites
+            para que hagas de esta una
+            experiencia que marque tu vida como profesional de la web.
+          </h3>
+          <p className="text-center mb-5">
+            <a href="#"
+               className="btn btn-outline-warning btn-lg">Contacto</a>
+          </p>
+
+        </div>
+      </div>
+    </div>
+  </section>;
+};
+
+export default HomeQuestions;
