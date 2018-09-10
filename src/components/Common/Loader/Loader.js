20c82100cad8fed1a64159c7fddadfdcebbe0182
Alex Arriaga <alexzander.arriaga@base22.com>
2018-09-10 11:39:27 -0500

    Arreglar convención de nombrado para parámetros stroke

--- src/components/Common/Loader/Loader.js
+++ src/components/Common/Loader/Loader.js
@@ -10,7 +10,7 @@ const Loader = (props) => {
         </div>
         <svg className="spinner" viewBox="25 25 50 50">
           <circle className="path" cx="50" cy="50" r="20" fill="none"
-                  stroke-width="2" stroke-miterlimit="10"/>
+                  strokeWidth="2" strokeMiterlimit="10"/>
         </svg>
       </div>
   );
